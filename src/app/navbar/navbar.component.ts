586b48fd353a80922b509098ba8ad7cb9235dd0f
jonathan <jonathan.willian321@gmail.com>
2024-11-13 19:05:19 -0300

    adcionada navbar

---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [RouterModule, ButtonModule],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css'
+})
+export class NavbarComponent {
+
+}
