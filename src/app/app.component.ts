03870364b829c3990c0bce9808ec89ec8d046f99
jonathan <jonathan.willian321@gmail.com>
2024-11-20 19:06:48 -0300

    ajeitado posicao do carrinho

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CarrinhoComponent } from './carrinho/carrinho.component';
 import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    RouterOutlet, NavbarComponent
+    RouterOutlet, NavbarComponent, CarrinhoComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
